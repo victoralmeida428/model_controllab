f081aa01d5e17a6ee07e34bdbe42987330cccfb9
victorgomes <victor.almeida@unigranrio.br>
2025-06-03 11:46:04 -0300

    update participantes para []int

--- pydata/ep_dw/parceiro_rede.go
+++ pydata/ep_dw/parceiro_rede.go
@@ -1,8 +1,8 @@
 package pydata
 
 type ParceiroRede struct {
-	ID            int      `db:"id" json:"id"`
-	Login         string   `db:"login" json:"login"`
-	Participantes []string `db:"participantes" json:"participantes"`
-	Senha         string   `db:"senha" json:"senha"`
+	ID            int    `db:"id" json:"id"`
+	Login         string `db:"login" json:"login"`
+	Participantes []int  `db:"participantes" json:"participantes"`
+	Senha         string `db:"senha" json:"senha"`
 }
