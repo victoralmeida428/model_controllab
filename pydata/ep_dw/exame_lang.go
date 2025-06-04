22a3729bd37c7acc262e37f4b7af6710e7b4e520
victorgomes <victor.almeida@unigranrio.br>
2025-06-04 13:51:23 -0300

    nome certificado null

--- pydata/ep_dw/exame_lang.go
+++ pydata/ep_dw/exame_lang.go
@@ -1,11 +1,11 @@
 package pydata
 
 type ExameLang struct {
-	IDModulo        int    `json:"id_modulo" db:"id_modulo"`
-	IDExame         int    `json:"id_exame" db:"id_exame"`
-	Lang            string `json:"lang" db:"lang"`
-	Nome            string `json:"nome" db:"nome"`
-	NomeCertificado string `json:"nome_certificado" db:"nome_certificado"`
-	ID              int    `json:"id" db:"id"`
-	IDModExame      int    `json:"id_mod_exame" db:"id_mod_exame"`
+	IDModulo        int     `json:"id_modulo" db:"id_modulo"`
+	IDExame         int     `json:"id_exame" db:"id_exame"`
+	Lang            string  `json:"lang" db:"lang"`
+	Nome            string  `json:"nome" db:"nome"`
+	NomeCertificado *string `json:"nome_certificado" db:"nome_certificado"`
+	ID              int     `json:"id" db:"id"`
+	IDModExame      *int    `json:"id_mod_exame" db:"id_mod_exame"`
 }
