c1ef88e800f5fa484ad2d0105dd11f3166d32340
victorgomes <victor.almeida@unigranrio.br>
2025-06-04 11:44:29 -0300

    add table lang

---
+++ pydata/ep_dw/modulo_lang.go
@@ -0,0 +1,9 @@
+package pydata
+
+type ModuloLang struct {
+	IDModulo        int    `json:"id_modulo" db:"id_modulo"`
+	Lang            string `json:"lang" db:"lang"`
+	Nome            string `json:"nome" db:"nome"`
+	NomeCertificado string `json:"nome_certificado" db:"nome_certificado"`
+	ID              int    `json:"id" db:"id"`
+}
