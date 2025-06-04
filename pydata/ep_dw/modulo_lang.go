ef247f41813679f33552869be835ff93deb8d326
victorgomes <victor.almeida@unigranrio.br>
2025-06-04 13:01:13 -0300

    NomeCertificado string `json:"nome_cert" db:"nome_certificado"`

--- pydata/ep_dw/modulo_lang.go
+++ pydata/ep_dw/modulo_lang.go
@@ -4,6 +4,6 @@ type ModuloLang struct {
 	IDModulo        int    `json:"id_modulo" db:"id_modulo"`
 	Lang            string `json:"lang" db:"lang"`
 	Nome            string `json:"nome" db:"nome"`
-	NomeCertificado string `json:"nome_certificado" db:"nome_certificado"`
+	NomeCertificado string `json:"nome_cert" db:"nome_certificado"`
 	ID              int    `json:"id" db:"id"`
 }
